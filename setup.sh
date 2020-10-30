60fff611e5e5be66a9a8838fbfa012e3dd589544
duckish <nguyen_vanduc@hotmail.com>
2020-10-30 13:25:31 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,14 +3,9 @@
 apt update && apt install -y curl git openjdk-11-jdk
 
 ### go to this https://kafka.apache.org/downloads to check for new version
-#curl https://mirror.its.dal.ca/apache/kafka/2.4.1/kafka_2.13-2.4.1.tgz > all_files.tgz
-
 curl https://mirror.its.dal.ca/apache/kafka/2.6.0/kafka_2.13-2.6.0.tgz > all_files.tgz
 
 tar xzf all_files.tgz
-
-#EXTRACTED=`ls | grep kafka`
-#mkdir -p /opt/kafka
 mv kafka_2.13-2.6.0 /opt/kafka
 
 ### set up services
